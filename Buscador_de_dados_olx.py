0eaaac7eb460f662baffbf72a9a9a0bd1fa46a7f
FranciscoBurigo <franciscoburigo@gmail.com>
2022-02-27 23:11:54 -0300

    Buscador_de_dados_olx.py

--- Buscador_de_dados_olx.py
+++ Buscador_de_dados_olx.py
@@ -12,6 +12,7 @@ import time
 from datetime import date
 import re
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 
 ListaURLS = []
@@ -19,10 +20,11 @@ listaJson =[]
 Dictdt= []
 Dictdd= []
 
+
 #busca URLS dos anuncios e salva em uma lista
-def buscaURLsOLX(pages = 3):
-    #https://sc.olx.com.br/florianopolis-e-regiao/imoveis/terrenos/compra
+def buscaURLsOLX(pages = 10):
     
+    ErroLink = 0
     
     for x in range(1, pages):
         
@@ -46,94 +48,85 @@ def buscaURLsOLX(pages = 3):
                 ListaURLS.append(URL_anuncio)
                 
             except:
-                print("erro")
+                ErroLink = ErroLink + 1
             
 
             
 
-        
-
+    N_Pag = len(ListaURLS)   
+    print("O numero de links coletados foi:"+ str(N_Pag) + ", não conseguindo coletar um total de " + str(ErroLink))
     return(ListaURLS)
 
 
 def buscaDadosOLX(ListaURLS):
     N_Pag = len(ListaURLS)
 
-    for x in range(0, 2):
+    for x in range(0, N_Pag):
         
-        #url = "https://sc.olx.com.br/florianopolis-e-regiao/terrenos/terreno-990m-de-esquina-996439294"
+        
         url = ListaURLS[x]
         
-            
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'}
-        
-
-        #chrome_options = Options()
-        # maximized window
-        #chrome_options.add_argument("--start-maximized")
-        
-        #page = requests.get(url = url, headers=headers)
-        browser = webdriver.Chrome()
-        browser.get(url)
-        
-        browser.maximize_window()
-        
-        time.sleep(5)
-        html = browser.page_source
-        #soup = BeautifulSoup(page.content,'lxml')
-        soup = BeautifulSoup(html,'lxml')
-        #print(soup)
-        
-        Titulo = soup.find("h1",{"class" : "sc-45jt43-0 eCghYu sc-ifAKCX cmFKIN"}).get_text()
-        Preco = soup.find("h2",{"class" : "sc-1wimjbb-2 iUSogS sc-ifAKCX cmFKIN"}).get_text()
-        Preco = Preco.split("R$")[1]
-        Preco = float(Preco.replace(".",""))
-        Descricao = soup.find("span",{"class" : "sc-1sj3nln-1 eOSweo sc-ifAKCX cmFKIN"}).get_text()
-        NomeVendedorURL = soup.find("span",{"class": "sc-fBuWsC sc-cnTzU hWZqZT sc-hARARD enkigN sc-ifAKCX kanYPz"}).get_text()
-        ddclass = soup.find_all("dd",{"class": "sc-1f2ug0x-1 ljYeKO sc-ifAKCX kaNiaQ"})
-        dtclass = soup.find_all("dt",{"class": "sc-1f2ug0x-0 cLGFbW sc-ifAKCX cmFKIN"})
-        #teste = soup.find("script", {"id" : "initial-data"})
-        #print(teste)
-        #data = json.loads(teste.get_text())
-        print(NomeVendedorURL)
-##
-##        for a in NomeVendedorURL:
-##            try:
-##                URL_vendedor = a.find("a")["href"]
-##
-##                print(URL_vendedor)
-##                
-##                
-##            except:
-##                print("erro")
-##    
-        
-
+        try:    
        
-        for a in range(0, len(ddclass)):
-            Dictdt.append(dtclass[a+ 1].get_text())
-            Dictdd.append(ddclass[a].get_text())
+        
 
-        dict_tamanho_localizacao = dict(zip(Dictdt, Dictdd))
+            chrome_options = Options()
+            
+            
+            chrome_options.add_argument("--headless")
+            
+            
+            s = Service('C:/Users/franc/OLX_scrapper/chromedriver.exe')
+            browser = webdriver.Chrome(service=s, options = chrome_options)
+        
+        
+        
+            browser.get(url)
+            time.sleep(5)
+            html = browser.page_source
+            soup = BeautifulSoup(html,'lxml')
 
-        json = {"Titulo" : Titulo,
-                "Preco" : Preco,
-                "NomeVendedor" : NomeVendedorURL,
-                "Descricao" : Descricao,
+            
+            Titulo = soup.find("h1",{"class" : "sc-45jt43-0 eCghYu sc-ifAKCX cmFKIN"}).get_text()
+            Preco = soup.find("h2",{"class" : "sc-1wimjbb-2 iUSogS sc-ifAKCX cmFKIN"}).get_text()
+            Preco = Preco.replace("R$","")
+            Preco = float(Preco.replace(".",""))
+            Descricao = soup.find("span",{"class" : "sc-1sj3nln-1 eOSweo sc-ifAKCX cmFKIN"}).get_text()
+            NomeVendedorURL = soup.find("span",{"class": "sc-fBuWsC sc-jGFFOr iNEwhQ sc-hARARD ksmUnv sc-ifAKCX cmFKIN"}).get_text()
+            ddclass = soup.find_all("dd",{"class": "sc-1f2ug0x-1 ljYeKO sc-ifAKCX kaNiaQ"})
+            dtclass = soup.find_all("dt",{"class": "sc-1f2ug0x-0 cLGFbW sc-ifAKCX cmFKIN"})
+            
+           
+            for a in range(0, len(ddclass)):
+                Dictdt.append(dtclass[a+ 1].get_text())
+                Dictdd.append(ddclass[a].get_text())
+
+            dict_tamanho_localizacao = dict(zip(Dictdt, Dictdd))
+
+            json = {"Titulo" : Titulo,
+                    "Preco" : Preco,
+                    "NomeVendedor" : NomeVendedorURL,
+                    "Descricao" : Descricao,
+                    "URL_Anuncio": url
+                    
+
+                }
+            json.update(dict_tamanho_localizacao)
+        
+        
+
+        
+            listaJson.append(json)
+
+            browser.close()
                 
+        except:
+            print("Erro para pegar dados")
 
-            }
-        json.update(dict_tamanho_localizacao)
 
+            
         
-
         
-        listaJson.append(json)
-
-
-        
-
-        browser.close()
         
         
 
